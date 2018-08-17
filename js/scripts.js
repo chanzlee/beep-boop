af56165fe6607d197b736d0ae11d1d9e29fbf0e5
Chan Lee <chanethanlee@gmail.com>
2018-08-17 11:18:17 -0700

    edit function for isThree variable with proper condition.

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,7 @@ $(document).ready(function(){
     event.preventDefault();
 
     var userInput = parseInt($("input#numberInput").val());
+    reversed = $("input:checkbox[name=order]:checked").length;
     var result = beepBoop(userInput);
     $("#result").text(result);
 
@@ -17,32 +18,44 @@ function beepBoop (numberInput) {
 
   for (var number = 0; number <= numberInput; number++) {
     strNum = number.toString();
-    alert(strNum);
     isZero = false;
     isOne = false;
     isThree = false;
 
-    for ( i = 0; i < strNum.length; i++) {
-      var  checkNum= strNum.charAt(i);
+    // check all numbers to find whether it inclues 0, 1, number divisible by 3. When it finds, return true for revelant boolean variable.
 
-      if (checkNum == '0') {
-        isZero = true;
-      } else if ( checkNum == '1') {
-        isOne = true;
-      } else if ( checkNum == '3') {
-        isThree = true;
-      }
+    if (number % 3 === 0){
+      isThree = true;
     }
+
+    for ( i = 0; i < strNum.length; i++) {
+      var checkNum = strNum.charAt(i);
+        if ( checkNum === "1") {
+        isOne = true;
+        } else if (checkNum === "0") {
+        isZero = true;
+        }
+    }
+
+    // push results for each condition met, filtering from the higher priority condition.
 
     if (isThree) {
-      result += "I'm sorry, Dave. I'm afraid I can't do that. ";
+      result.push("I'm sorry, Dave. I'm afraid I can't do that.");
     } else if (isOne) {
-      result += "Boop! ";
+      result.push("Boop!");
     } else if (isZero) {
-      result += "Beep! ";
+      result.push("Beep!");
     } else {
-      result += strNum + " ";
+      result.push(strNum);
     }
+  }
+
+  // In the if condition below, "reversed" will result 1 or true if checkbox was checked, otherwise result 0 or false.
+
+  if (reversed) {
+    result = result.reverse().join(" ")
+  } else {
+    result =  result.join(" ")
   }
   return result;
 };
