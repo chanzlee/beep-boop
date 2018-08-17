723f0aa6e70bec158bc1f7c4dd1049ec5ee2bf99
Chan Lee <chanethanlee@gmail.com>
2018-08-17 09:37:38 -0700

    edit js to print relevant output for special numbers.

--- js/scripts.js
+++ js/scripts.js
@@ -17,21 +17,31 @@ function beepBoop (numberInput) {
   for (var number = 0; number <= numberInput; number++) {
     strNum = number.toString();
     alert(strNum);
-    isSpecial = false;
+    isZero = false;
+    isOne = false;
+    isThree = false;
 
     for ( i = 0; i < strNum.length; i++) {
-        var  checkNum= strNum.charAt(i);
-        if (checkNum == '0' || checkNum == '1' || checkNum == '3') {
-            isSpecial = true;
-        }
+      var  checkNum= strNum.charAt(i);
+
+      if (checkNum == '0') {
+        isZero = true;
+      } else if ( checkNum == '1') {
+        isOne = true;
+      } else if ( checkNum == '3') {
+        isThree = true;
+      }
     }
 
-    if (isSpecial) {
-      result += "special"
+    if (isThree) {
+      result += "I'm sorry, Dave. I'm afraid I can't do that.";
+    } else if (isOne) {
+      result += "Boop!";
+    } else if (isZero) {
+      result += "Beep!";
     } else {
-      result += strNum
+      result += strNum;
     }
-
   }
   return result;
 };
